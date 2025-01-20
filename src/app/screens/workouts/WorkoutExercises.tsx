c2cfc09d2b87ddc59b84aa37d3975899f6f5e5df
ericserka <ericserka@hotmail.com>
2025-01-20 20:27:48 -0300

    feat: workout exercises screen

--- src/app/screens/workouts/WorkoutExercises.tsx
+++ src/app/screens/workouts/WorkoutExercises.tsx
@@ -1,3 +1,88 @@
+import { CustomButton } from "@/app/components/CustomButton"
+import { FlatListOfCards } from "@/app/components/FlatListOfCards"
+import { onDelete } from "@/app/helpers/alert"
+import { useStore } from "@/app/helpers/store"
+import { useWorkoutExercisesDb } from "@/app/hooks/useWorkoutExercisesDb"
+import { Workout } from "@/lib/types/workout"
+import { useSQLiteContext } from "expo-sqlite"
+import { pipe } from "fp-ts/function"
+import * as O from "fp-ts/Option"
+import { useEffect } from "react"
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native"
+
 export const WorkoutExercises = () => {
-  return <></>
+  const {
+    workout,
+    navigate,
+    setWorkoutExercise,
+    clearWorkoutExercise,
+    fallbackToHome
+  } = useStore()
+  const { list, loading, workoutExercises, remove } = useWorkoutExercisesDb()
+  const db = useSQLiteContext()
+
+  useEffect(() => {
+    if (O.isSome(workout)) {
+      list(db, workout.value.id)
+    }
+  }, [workout])
+
+  const SomeWorkout = ({ w }: { w: Workout }) => {
+    if (loading.query) {
+      return <ActivityIndicator size="large" />
+    }
+
+    return (
+      <View>
+        <CustomButton
+          title={"Add a new exercise to this workout"}
+          disabled={loading.mutation}
+          onPress={() => {
+            clearWorkoutExercise()
+            navigate("workoutExerciseForm")
+          }}
+        />
+        <Text style={styles.title}>{`${w.name} exercises`}</Text>
+        <FlatListOfCards
+          data={workoutExercises}
+          keyExtractor={item => item.id.toString()}
+          title={item => item.exerciseName}
+          subtitle={item => `${item.sets}x ${item.reps} ${item.weight ? `- ${item.weight}kg` : ""}`}
+          actionsDisabled={loading.mutation}
+          onCardPress={item => {
+            setWorkoutExercise(item)
+            navigate("workoutExercise")
+          }}
+          onDeletePress={item => {
+            onDelete("Delete Exercise", () => remove(db, item.id))
+          }}
+          onEditPress={item => {
+            setWorkoutExercise(item)
+            navigate("workoutExerciseForm")
+          }}
+        />
+      </View>
+    )
+  }
+
+  const onNone = () => {
+    fallbackToHome()
+
+    return <></>
+  }
+
+  const onSome = (w: Workout) => {
+    return <SomeWorkout w={w} />
+  }
+
+  return pipe(workout, O.match(onNone, onSome))
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 12
+  }
+})
