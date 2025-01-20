a3481838ecb879e1129aedebe11eb30b9ba8d66e
ericserka <ericserka@hotmail.com>
2025-01-20 20:29:43 -0300

    feat: isEmpty string helper

--- src/lib/helpers/string.ts
+++ src/lib/helpers/string.ts
@@ -3,3 +3,7 @@ import * as S from "fp-ts/string"
 
 export const camelToSnake = (str: string) =>
   pipe(str, S.replace(/([a-z])([A-Z])/g, "$1_$2"), S.toLowerCase)
+
+export const isEmpty = (str: string) => pipe(str, S.trim, S.isEmpty)
+
+export const isNotEmpty = (str: string) => !isEmpty(str)
