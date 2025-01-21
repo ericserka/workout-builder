6416375f72c524fe400c422068059d455af0bdf3
ericserka <ericserka@hotmail.com>
2025-01-21 20:23:02 -0300

    feat: add more array helpers

--- src/lib/helpers/array.ts
+++ src/lib/helpers/array.ts
@@ -2,6 +2,7 @@ import * as A from "fp-ts/Array"
 import { pipe } from "fp-ts/function"
 import * as R from "fp-ts/Record"
 import * as O from "fp-ts/Option"
+import * as Eq from "fp-ts/Eq"
 
 export const join =
   <T>(separator: string) =>
@@ -25,4 +26,22 @@ export const groupBy: GroupBy =
           O.getOrElse(() => R.upsertAt(key, [item])(acc))
         )
       })
+    )
+
+type RemoveDuplicates = <A>(eq: Eq.Eq<A>) => (arr: A[]) => A[]
+export const removeDuplicates: RemoveDuplicates =
+  <A>(eq: Eq.Eq<A>) =>
+  (arr: A[]): A[] =>
+    pipe(
+      arr,
+      A.reduce<A, A[]>([], (acc, elem) =>
+        pipe(
+          acc,
+          A.findFirst(item => eq.equals(item, elem)),
+          O.match(
+            () => [...acc, elem],
+            () => acc
+          )
+        )
+      )
     )
