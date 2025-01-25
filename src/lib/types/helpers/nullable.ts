143fd5d1fdfb8800d8824f9a01d5c1844153a062
ericserka <ericserka@hotmail.com>
2025-01-25 12:51:12 -0300

    remove unused helpers

--- src/lib/types/helpers/nullable.ts
+++
@@ -1,4 +0,0 @@
-import * as t from "io-ts"
-
-export const Nullable = <C extends t.Mixed>(codec: C) =>
-  t.union([codec, t.null])
--- src/lib/types/helpers/optional.ts
+++
@@ -1,4 +0,0 @@
-import * as t from "io-ts"
-
-export const Optional = <C extends t.Mixed>(codec: C) =>
-  t.union([codec, t.undefined])
